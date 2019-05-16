bdc8ffaa49f35ac065c643c14194295ddbe34efe
mstfakdgn <mustafa.akiler@gmail.com>
2019-05-16 14:54:14 +0300

    video 12 relationship between post ,tag and category tables

--- app/Model/user/post.php
+++ app/Model/user/post.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class post extends Model
 {
-    //
+    public function tags(){
+
+        return $this->belongstoMany('App\Model\user\tag','post_tags');
+
+    }
+    public function categories(){
+
+        return $this->belongstoMany('App\Model\user\category','category_posts');
+
+    }
 }
--- app/Model/user/post_tags.php
+++ app/Model/user/post_tags.php
@@ -4,7 +4,7 @@ namespace App\Model\user;
 
 use Illuminate\Database\Eloquent\Model;
 
-class post_tag extends Model
+class post_tags extends Model
 {
     //
 }
