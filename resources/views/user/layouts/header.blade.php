e8da8322af732cf93b9208e3d38ef11175da7102
mstfakdgn <mustafa.akiler@gmail.com>
2019-05-23 09:47:20 +0300

    admin tables super admin hash, seeder

--- resources/views/user/layouts/header.blade.php
+++ resources/views/user/layouts/header.blade.php
@@ -32,6 +32,7 @@
 
             <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                 @csrf
+                
             </form>
            @endif
           </li>
